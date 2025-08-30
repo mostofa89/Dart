4e5764aabe15b13e4cd4e12ed49f619a2c6685f3
mostofa89 <mostofakamaljoy89@gmail.com>
2025-08-31 03:11:06 +0600

    Add Basic Directory and variables

---
+++ Fundamentals/basics/variable.dart
@@ -0,0 +1,16 @@
+import 'dart:ffi';
+
+void main(){
+  int a = 5;
+  String str = "Hello";
+  double d = 3.1416;
+  bool isTrue = false;
+  List<int> nums = [1, 2, 3, 4, 5];
+  Object name = "Mostofa";
+  print(a);
+  print(str);
+  print(d);
+  print(isTrue);
+  print(nums);
+  print(name);
+}
